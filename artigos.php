9af5c4f128bb209f73156910079559da47002441
Thiago Vargas <thiagovars@gmail.com>
2015-04-03 15:44:00 -0300

    Separado os artigos para melhor exibição dos artigos.

--- artigos.php
+++ artigos.php
@@ -25,181 +25,71 @@ include_once('header.php');
 
 				<div class="container">
 
-					<div class="row">
-						<div class="col-md-12" data-appear-animation="fadeInUp">
-							<h3><strong>Artigo:</strong> As 7 virtudes do samurai na prática do aikido</h3>
-							<p class="drop-caps">Um dos motivos que diferenciam o Aikido das demais artes marciais é o fato de
-							manter as tradições de etiqueta da antiga cultura marcial. O Aikido enquanto Budô, e 
-							não luta esportiva, busca algo mais para os praticantes do que uma medalha ou troféu. 
-							Esta busca pela evolução dos aikidoístas é pautada pelo antigo código de conduta dos 
-							Samurais, também conhecido como Bushido.</p>
+					<?php 
+					$artigo = $_REQUEST['art'];
 
-							<p class="tall">Um dos preceitos salientados no Bushido, refere-se às virtudes dos samurais,
-							em número de sete. Estas sete virtudes são trazidas, enquanto elemento simbólico, às 
-							práticas do aikido por meio da representação das sete pregas do Hakama, adotado como 
-							indumentária dos graduados.</p>
+					switch ($artigo) {
+						case '7virtudes':
+							include_once('7virtudes.php');
+							echo '<hr class="tall">';
+							break;
 
-							<p class="tall">Abaixo discorro sobre cada uma destas virtudes, e sua correspondência com a
-							prática do Aikido nos tempos atuais:</p>
+						case 'giri':
+							include_once('giri.php');
+							echo '<hr class="tall">';
+							break;
 
-							<p class="tall">1a GI: pode ser entendida como <strong>dever</strong>, obrigação, mas também retidão, integridade,
-							justiça e moralidade, como os termos abaixo derivados:</p>
-							<ul>
-								<li><strong>Gimu: dever, obrigação;</strong></li>
-								<li><strong>Giri: Sentimento de dívia, gratidão;</strong></li>
-								<li><strong>Giri gatai hito: Pessoa cumpridora dos deveres sociais;</strong></li>
-								<li><strong>Seigui: Justiça, Caminho da Verdade. (1)</strong></li>
-							</ul>
-
-							<p class="tall">2a YUU: traduzida como coragem, pode também ser entendida como bravura. Neste
-							sentido vemos que no desenvolvimento do Aikido, o postulante deve superar a si 
-							mesmo e as suas próprias fraquezas. Se recuar diante dos desafios que se põem à sua 
-							frente, muitas vezes abandona o caminho por medo de persistir. A coragem é assim 
-							representada pela conduta de persistir, apesar do medo, dos receios e de suas próprias 
-							fragilidades. Emblemáticos são os exames de faixa onde os deshis são testados não 
-							apenas física, mas psicologicamente, expondo-se à apreciação de uma banca avaliadora. 
-							Sem bravura ou coragem, não seria possível trilhar o caminho do desenvolvimento no 
-							Aikido.</p>
-							<p class="tall">3a. JIN: benevolência. A benevolência ou compaixão para um genuíno praticante de
-							Aikido é uma virtude essencial e se manifesta pela capacidade de colocar-se, de forma 
-							generosa, no lugar dos seus companheiros, por meio do sentimento de auxílio à sua 
-							evolução. Isso é representado pela atitude frente ao seu “oponente”, ou melhor, dizendo, 
-							companheiro de técnica (seja atuando como uke ou nague), com vistas a evitar qualquer 
-							forma de dano a outrem. O verdadeiro aikidoísta possui o espírito do altruísmo: prefere 
-							machucar a si mesmo do que ao colega. Além disso, de forma benevolente busca 
-							contribuir para o desenvolvimento dos menos graduados ou dos colegas com maiores 
-							dificuldades.</p>
-
-							<p class="tall">4a REI: respeito, cortesia, polidez. Aplicada à conduta do aikidoísta denota a reverência
-							que este possui com todos os elementos envolvidos na sua prática. Ao cumprimentar, 
-							de forma reverente e cortez, o altar (kamiza), bem como seu Sensei e cada um de seus 
-							colegas, sejam eles mais ou menos graduados, o aikidoísta demonstra seu respeito por 
-							todos e tudo o que permite o seu próprio aprimoramento. O REI pontua o início e o fim 
-							de cada trabalho ou técnica, por meio do cumprimento. Toda a “etiqueta” do Aikido, 
-							com vistas ao respeito à prática e aos objetos de trabalho, também fazem parte deste 
-							conceito.</p>
-
-							<p class="tall">5a. MAKOTO: sinceridade. A intenção e aplicação dos valores e virtudes deve ocorrer 
-							por meio de relações galgadas na verdade de coração. Nesse sentido, aplicado ao aikido, 
-							vejo a verdade do praticante em, de forma sincera, querer sua evolução, dos demais 
-							companheiros e do grupo. O termo verdade também pode ser obtido pela combinação 
-							das palavras japonesas guen (palavra) e jo (perfeito). Nessa óptica, a palavra do 
-							aikidoísta, também deve ser mantida, a fim de manter compromissos assumidos com o 
-							grupo e consigo mesmo. Como afirmou Inazos Nitobe: “Sem veracidade e sinceridade 
-							a polidez é uma farsa e uma representação” (2).</p>
-
-							<p class="tall">6a. MEIYO: honra. Podemos aplicar essa virtude, de forma diferente do orgulho, à
-							valorização de si e do grupo e linhagem a qual está inserido. Em detrimento de qualquer 
-							caráter comparativo com os demais grupos ou artes, o aikidoísta valoriza seu grupo 
-							e hierarquia pela consciência do valor e da importância dessa estrutura para o seu 
-							caminho (do). Como exemplo, cito a honra que sentimos por fazer parte do grupo do 
-							maior mestre que o Brasil já teve, dedicando sua vida e seus esforços para que o aikidô 
-							brasileiro se encontre no atual patamar, Kawai shihan.</p>
-
-							<p class="tall">7a. CHU: lealdade. Tanto o budismo quanto os preceitos confuncianos tornaram a
-							obediência aos pais como a primeira dívida do ser humano. “O bushido afirma que 
-							o interesse da família e dos seus membros é intacta, una e inseparável.”(3). O ápice 
-							desse leadade era anteriormente representado pelo seppuku (harakiri). Na prática do 
-							aikido, entendo esse conceito como o dever de fidelidade e lealdade ao grupo, sendo 
-							leal aos seus superiores e a própria preservação da técnica, evitando sua distorção ou 
-							disseminação indevida. Como recomendava Ô Sensei, <strong>“não ensinar a arte à perversos, 
-							que possam fazer da arte motivo de destruição”</strong>.</p>
-
-							<p class="tall">Vejo assim que, embora sendo preceitos de séculos passados, as virtudes dos
-							samurais ainda se mantém preservadas na prática do Aikido dos dias atuais. É também 
-							por isso que diferenciamos nossa Arte Marcial de tantas outras, e devemos nos esforçar 
-							para manter viva essa tradição.</p>
-
-
-							<p class="tall">(1) YAMAMOTO, Yashiro. <strong>As Sete Virtudes do Bushido</strong> in Kendo: o caminho da 
-							espada. São Paulo: Online, __ (p.31)
-							(2) IBID. _ (p. 32)
-							(3) IBID. _ (p. 33)</p>
-						</div>						
-					</div>
-
-					<hr class="tall">
-
-					<div class="row">
-						<div class="col-md-12" data-appear-animation="fadeInUp">
-							<h3><strong>Giri:</strong> A arte da gratidão</h3>
-							<p class="tall" style="text-align: right;">por Angélica Djenane Philippe</p>
-							<p class="tall" style="text-align: right;">Sensei Eduardo Mendes Ribeiro Correa1</p>
-
-							<div style="float: right; width: 100%;">
-								<p class="tall" style="text-align: justify; width: 430px; font-style: italic; float: right;">“A gratidão é uma oração sincera, um sentimento profundo de
-								reverência pelo caminho e pelo modo de vida do Aikidô. Quando a 
-								ferramenta se desgastava ela não era simplesmente jogada fora, mas 
-								formalmente recolhida numa cerimônia especial
-								longo e leal serviço.” (Morihei Ueshiba)</p>
-							</div>
-							<p class="tall">A pós-modernidade teve, como uma de suas características marcantes, na esfera
-							comportamental, o estímulo à competitividade e ao individualismo. Os resultados não nos 
-							fizeram necessariamente uma sociedade mais feliz e realizada. Em contraposição, novos 
-							paradigmas nos convidam a uma revisão de valores e nos levam a perceber que estamos 
-							inseridos em uma teia de relações, onde cada ação pontual pode gerar impactos em uma escala 
-							maior, cujos limites não podemos precisar.</p>
-													<p class="tall">Com raízes em teorias científicas da química, física ou biologia, novas referências
-							contribuem para a ruptura do paradigma reducionista, construindo as bases para a compreensão 
-							da complexidade dos fenômenos. Passamos assim a compreender a “teia” que interconecta 
-							todos os seres e os elementos do planeta, verificando que pequenos fatos, aparentemente 
-							irrelevantes, podem ter resultados de significativas conseqüências.</p>
-													<p class="tall">Vivemos em um mundo de relações, onde a forma como interagimos com nossos pares
-							passa a ser uma variável significativa para o sucesso ou insucesso das “organizações”, sejam 
-							elas associações, família, empresas. A partir do momento em que nos percebemos como parte 
-							de um todo, verificamos que a forma como consolidamos nossas relações é substancialmente 
-							importante para o êxito de nossos projetos e empreendimentos. Neste sentido, acreditamos que 
-							um valor é fundamentalmente importante: o respeito, sobretudo aquele galgado na gratidão.</p>
-													<p class="tall">Neste sentido destacamos um termo de origem nipônica que, na nossa compreensão,
-							representa eficazmente este conceito: “GIRI”
-							equivocadamente apropriado para promover violências e autoritarismos3
-							ao seu sentido mais nobre, onde é compreendido como “gratidão”, ou “respeito”, ou, melhor 
-							ainda, “o respeito galgado na gratidão”.</p>
-													<p class="tall">Falamos aqui na consideração e sentimento de gratidão que devemos ter por todos
-							aqueles que fazem algo em nosso benefício, muitas vezes algo que nós mesmos não poderíamos 
-							fazer: da pessoa que zelosamente prepara o café, a que mantém limpos nossos cômodos, aos 
-							nossos chefes e líderes que nos mostraram caminhos, assumiram responsabilidades.</p>
-													<p class="tall">Acreditamos que é importante dedicarmos o “GIRI”, sobretudo, àqueles que nos
-							ensinaram algo, muitas vezes colocando ao nosso dispor, de forma suave, conhecimentos 
-							que exigiram de si dedicação e sacrifico para serem obtidos. Quem nos doa um de seus bens 
-							mais preciosos: o tempo, a dedicação e o saber, nos conduzem a um infinito compromisso de 
-							gratidão.</p>
-													<p class="tall">Não são raras as vezes que os vícios humanos do egoísmo e do orgulho nos ofuscam
-							a percepção do quanto devemos aos que nos cercam, e, sobretudo, aos nossos orientadores, 
-							obstaculizando, pela exacerbação do individualismo, nossa compreensão do “prazer de servir”. 
-							Muitas vezes, a obediência e o respeito são percebidos de forma depreciativa, traduzidos como 
-							subserviência e como uma fraqueza de caráter em nossa sociedade competitiva.</p>
-													<p class="tall">Contrariamente, os samurais, a classe guerreira do Japão feudal, tinham este conceito
-							como base de suas atividades. Em seu sentido etimológico, a própria expressão relaciona-se ao 
-							ato de servir, e ainda assim os samurais são associados à imagem de coragem e autoridade.</p>
-													<p class="tall">Pensamos que servir com amor e respeito é uma forma de honrar àqueles a quem somos
-							gratos, estabelecendo relações harmoniosas com todas as pessoas. Ao contrário de representar 
-							sacrifício e exploração, o bem servir nos conduz ao sentimento de realização, de cumprimento 
-							de nossa missão.</p>
-													<p class="tall">Nosso sensei nos ensina que devemos lembrar com carinho e gratidão do legado de
-							nosso shihan, tendo por ele o mesmo sentimento que temos por um ancestral. Se nossos mestres 
-							são como pais no AIKIDÔ, os mestres de nossos mestres são nossos avós. Este respeito é 
-							demonstrado pela devoção ao AIKIDÔ, pelo amor na sua prática, e pela busca de União entre 
-							todos os federados.</p>
-													<p class="tall">Se nossas relações, sejam elas hierárquicas ou horizontais, forem galgadas sobre os
-							conceitos de “GIRI” teremos dojos mais saudáveis, sustentados pelo respeito e gratidão mútuos, 
-							permitindo que o sentimento de parceria seja potencializado, e, por decorrência, obteremos a 
-							formação de equipes com melhores desempenhos, mais realizadas em suas atividades e com a 
-							harmonia que tanto buscamos desenvolver em nossa Arte.</p>
-							<div class="nota-rodape"></div>
-							<p class="tall">¹ Presidente da Associação KOBUKAN.<BR>
-							² <strong>Justiça, obrigação</strong>. in Dicionário Japonês-Português São Paulo: Ed. Rideel)<BR>
-							³ <a href="http://japanese.about.com/od/japanesecultur1/a/071497.htm" target="_blank">
-								http://japanese.about.com/od/japanesecultur1/a/071497.htm
-						 	  </a> (30/jun/2011)
-						 	</p>
-						</div>
-
-						<div class="col-md-12" data-appear-animation="fadeInUp">
+						case 'espiritismo':
+							include_once('espiritismo.php');
+							echo '<hr class="tall">';
+							break;
 							
-						</div>
+						default:
+							?>
+							<div class="row">
+								<div class="col-md-12" data-appear-animation="fadeInUp">
+									<h3><strong>Artigo:</strong> As 7 virtudes do samurai na prática do aikido</h3>
+		              <p class="drop-caps">Um dos motivos que diferenciam o Aikido das demais artes marciais é o fato de
+		              manter as tradições de etiqueta da antiga cultura marcial. O Aikido enquanto Budô, e 
+		              não luta esportiva, busca algo mais para os praticantes do que uma medalha ou troféu. 
+		              Esta busca pela evolução dos aikidoístas é pautada pelo antigo código de conduta dos 
+		              Samurais, também conhecido como Bushido.</p>
+		              <a href="<?php echo $_SERVER['PHP_SELF'].'?art=7virtudes'; ?>">... Continuar lendo.</a>
+								</div>
+							</div>
+							<hr class="tall">
+							<div class="row">
+								<div class="col-md-12" data-appear-animation="fadeInUp">
+									<h3><strong>Giri:</strong> A arte da gratidão</h3>
+		              <p class="tall">A pós-modernidade teve, como uma de suas características marcantes, na esfera
+		              comportamental, o estímulo à competitividade e ao individualismo. Os resultados não nos 
+		              fizeram necessariamente uma sociedade mais feliz e realizada. Em contraposição, novos 
+		              paradigmas nos convidam a uma revisão de valores e nos levam a perceber que estamos 
+		              inseridos em uma teia de relações, onde cada ação pontual pode gerar impactos em uma escala 
+		              maior, cujos limites não podemos precisar.</p>
+		              <a href="<?php echo $_SERVER['PHP_SELF'].'?art=giri'; ?>">... Continuar lendo.</a>
+								</div>
+							</div>
+							<hr class="tall">
+							<div class="row">
+								<div class="col-md-12" data-appear-animation="fadeInUp">
+									<h3><strong>Aikido e o Espiritismo:</strong> Qual a relação?</h3>
+		              <p class="tall">Ueshiba concebeu o Aikido a partir da suas experiências com dezenas de artes marciais, mas principalmente 
+							    baseando-se no estilo daito-ryu aiki-jujutsu, do qual um dos principais expoentes foi Sokaku Takeda, ao qual incorporou 
+							    técnicas do kenjutsu (técnicas da espada) e do jojutsu (técnicas do bastão curto).</p>
 
-					</div>
+							      <p class="tall">Um importante atributo do Aikido é seu apego ao desenvolvimento espiritual.</p>
+
+							      <p class="tall">Na segunda metade da década de 1920, retornando à terra natal, Ueshiba conheceu e foi profundamente 
+							    influenciado por Onisaburo Deguchi, líder espiritual da religião Oomoto (uma espécie de movimento neo-xintoísta) em Ayabe³.</p>
+		              <a href="<?php echo $_SERVER['PHP_SELF'].'?art=espiritismo'; ?>">... Continuar lendo.</a>
+								</div>
+							</div>
+							<?php
+							break;
+					}
+
+					?>
 
 				</div>
 
